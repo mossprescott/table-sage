ba380825cd3cce604caccc0e4e72fa3f9e98cb9c
Moss Prescott <moss@theprescotts.com>
2026-03-14 18:50:13 -0400

    Results through today (Claude, struggling a bit)

--- src/Sage/EPL2025.elm
+++ src/Sage/EPL2025.elm
@@ -451,40 +451,420 @@ matches =
     , Round 18
         [ Day "Friday"
             "2025-12-26"
-            [ pending mun new
+            [ final mun 1 0 new
             ]
         , Day "Saturday"
             "2025-12-27"
-            [ pending nfo mci
-            , pending ars bha
-            , pending bre bou
-            , pending bur eve
-            , pending liv wol
-            , pending whu ful
-            , pending che avl
+            [ final nfo 1 2 mci
+            , final ars 2 1 bha
+            , final bre 4 1 bou
+            , final bur 0 0 eve
+            , final liv 2 1 wol
+            , final whu 0 1 ful
+            , final che 1 2 avl
             ]
         , Day "Sunday"
             "2025-12-28"
-            [ pending sun lee
-            , pending cry tot
+            [ final sun 1 1 lee
+            , final cry 0 1 tot
             ]
         ]
     , Round 19
         [ Day "Tuesday"
             "2025-12-30"
-            [ pending bur new
-            , pending che bou
-            , pending nfo eve
-            , pending whu bha
-            , pending ars avl
-            , pending mun wol
+            [ final bur 1 3 new
+            , final che 2 2 bou
+            , final nfo 0 2 eve
+            , final whu 2 2 bha
+            , final ars 4 1 avl
+            , final mun 1 1 wol
             ]
         , Day "Thursday"
             "2026-01-01"
-            [ pending cry ful
-            , pending liv lee
-            , pending bre tot
-            , pending sun mci
+            [ final cry 1 1 ful
+            , final liv 0 0 lee
+            , final bre 0 0 tot
+            , final sun 0 0 mci
+            ]
+        ]
+    , Round 20
+        [ Day "Saturday"
+            "2026-01-03"
+            [ final avl 3 1 nfo
+            , final bha 2 0 bur
+            , final wol 3 0 whu
+            , final bou 2 3 ars
+            ]
+        , Day "Sunday"
+            "2026-01-04"
+            [ final lee 1 1 mun
+            , final eve 2 4 bre
+            , final new 2 0 cry
+            , final tot 1 1 sun
+            , final ful 2 2 liv
+            , final mci 1 1 che
+            ]
+        ]
+    , Round 21
+        [ Day "Tuesday"
+            "2026-01-06"
+            [ final whu 1 2 nfo
+            ]
+        , Day "Wednesday"
+            "2026-01-07"
+            [ final bou 3 2 tot
+            , final bre 3 0 sun
+            , final cry 0 0 avl
+            , final eve 1 1 wol
+            , final ful 2 1 che
+            , final mci 1 1 bha
+            , final bur 2 2 mun
+            , final new 4 3 lee
+            ]
+        , Day "Thursday"
+            "2026-01-08"
+            [ final ars 0 0 liv
+            ]
+        ]
+    , Round 22
+        [ Day "Saturday"
+            "2026-01-17"
+            [ final mun 2 0 mci
+            , final che 2 0 bre
+            , final lee 1 0 ful
+            , final liv 1 1 bur
+            , final sun 2 1 cry
+            , final tot 1 2 whu
+            , final nfo 0 0 ars
+            ]
+        , Day "Sunday"
+            "2026-01-18"
+            [ final wol 0 0 new
+            , final avl 0 1 eve
+            ]
+        , Day "Monday"
+            "2026-01-19"
+            [ final bha 1 1 bou
+            ]
+        ]
+    , Round 23
+        [ Day "Saturday"
+            "2026-01-24"
+            [ final whu 3 1 sun
+            , final bur 2 2 tot
+            , final ful 2 1 bha
+            , final mci 2 0 wol
+            , final bou 3 2 liv
+            ]
+        , Day "Sunday"
+            "2026-01-25"
+            [ final bre 0 2 nfo
+            , final cry 1 3 che
+            , final new 0 2 avl
+            , final ars 2 3 mun
+            ]
+        , Day "Monday"
+            "2026-01-26"
+            [ final lee 1 1 eve
+            ]
+        ]
+    , Round 24
+        [ Day "Saturday"
+            "2026-01-31"
+            [ final bha 1 1 eve
+            , final lee 0 4 ars
+            , final wol 0 2 bou
+            , final che 3 2 whu
+            , final liv 4 1 new
+            ]
+        , Day "Sunday"
+            "2026-02-01"
+            [ final avl 0 1 bre
+            , final mun 3 2 ful
+            , final nfo 1 1 cry
+            , final tot 2 2 mci
+            ]
+        , Day "Monday"
+            "2026-02-02"
+            [ final sun 3 0 bur
+            ]
+        ]
+    , Round 25
+        [ Day "Friday"
+            "2026-02-06"
+            [ final lee 3 1 nfo
+            ]
+        , Day "Saturday"
+            "2026-02-07"
+            [ final mun 2 0 tot
+            , final bou 1 1 avl
+            , final ars 3 0 sun
+            , final bur 0 2 whu
+            , final ful 1 2 eve
+            , final wol 1 3 che
+            , final new 2 3 bre
+            ]
+        , Day "Sunday"
+            "2026-02-08"
+            [ final bha 0 1 cry
+            , final liv 1 2 mci
+            ]
+        ]
+    , Round 26
+        [ Day "Tuesday"
+            "2026-02-10"
+            [ final che 2 2 lee
+            , final eve 1 2 bou
+            , final tot 1 2 new
+            , final whu 1 1 mun
+            ]
+        , Day "Wednesday"
+            "2026-02-11"
+            [ final avl 1 0 bha
+            , final mci 3 0 ful
+            , final nfo 0 0 wol
+            , final cry 2 3 bur
+            , final sun 0 1 liv
+            ]
+        , Day "Thursday"
+            "2026-02-12"
+            [ final bre 1 1 ars
+            ]
+        ]
+    , Round 27
+        [ Day "Saturday"
+            "2026-02-21"
+            [ final avl 1 1 lee
+            , final bre 0 2 bha
+            , final che 1 1 bur
+            , final whu 0 0 bou
+            , final mci 2 1 new
+            ]
+        , Day "Sunday"
+            "2026-02-22"
+            [ final cry 1 0 wol
+            , final nfo 0 1 liv
+            , final sun 1 3 ful
+            , final tot 1 4 ars
+            ]
+        , Day "Monday"
+            "2026-02-23"
+            [ final mun 1 0 eve
+            ]
+        ]
+    , Round 28
+        [ Day "Friday"
+            "2026-02-27"
+            [ final wol 2 0 avl
+            ]
+        , Day "Saturday"
+            "2026-02-28"
+            [ final bou 1 1 sun
+            , final bur 3 4 bre
+            , final liv 5 2 whu
+            , final new 2 3 eve
+            , final lee 0 1 mci
+            ]
+        , Day "Sunday"
+            "2026-03-01"
+            [ final bha 2 1 nfo
+            , final ful 2 1 tot
+            , final mun 2 1 cry
+            , final ars 2 1 che
+            ]
+        ]
+    , Round 29
+        [ Day "Tuesday"
+            "2026-03-03"
+            [ final bou 0 0 bre
+            , final eve 2 0 bur
+            , final lee 0 1 sun
+            , final wol 2 1 liv
+            ]
+        , Day "Wednesday"
+            "2026-03-04"
+            [ final avl 1 4 che
+            , final bha 0 1 ars
+            , final ful 0 1 whu
+            , final mci 2 2 nfo
+            , final new 2 1 mun
+            ]
+        , Day "Thursday"
+            "2026-03-05"
+            [ final tot 1 3 cry
+            ]
+        ]
+    , Round 30
+        [ Day "Saturday"
+            "2026-03-14"
+            [ final bur 0 0 bou
+            , final sun 0 1 bha
+            , final ars 2 0 eve
+            , final che 0 1 new
+            , final whu 1 1 mci
+            ]
+        , Day "Sunday"
+            "2026-03-15"
+            [ pending cry lee
+            , pending mun avl
+            , pending nfo ful
+            , pending liv tot
+            ]
+        , Day "Monday"
+            "2026-03-16"
+            [ pending bre wol
+            ]
+        ]
+    , Round 31
+        [ Day "Wednesday"
+            "2026-02-18"
+            [ final wol 2 2 ars
+            ]
+        , Day "Friday"
+            "2026-03-20"
+            [ pending bou mun
+            ]
+        , Day "Saturday"
+            "2026-03-21"
+            [ pending bha liv
+            , pending ful bur
+            , pending eve che
+            , pending lee bre
+            ]
+        , Day "Sunday"
+            "2026-03-22"
+            [ pending new sun
+            , pending avl whu
+            , pending tot nfo
+            ]
+        ]
+    , Round 32
+        [ Day "Friday"
+            "2026-04-10"
+            [ pending whu wol
+            ]
+        , Day "Saturday"
+            "2026-04-11"
+            [ pending ars bou
+            , pending bre eve
+            , pending bur bha
+            , pending cry new
+            , pending nfo avl
+            , pending liv ful
+            ]
+        , Day "Sunday"
+            "2026-04-12"
+            [ pending sun tot
+            , pending che mci
+            ]
+        , Day "Monday"
+            "2026-04-13"
+            [ pending mun lee
+            ]
+        ]
+    , Round 33
+        [ Day "Saturday"
+            "2026-04-18"
+            [ pending bre ful
+            , pending avl sun
+            , pending lee wol
+            , pending new bou
+            , pending nfo bur
+            , pending tot bha
+            , pending che mun
+            ]
+        , Day "Sunday"
+            "2026-04-19"
+            [ pending eve liv
+            , pending mci ars
+            ]
+        , Day "Monday"
+            "2026-04-20"
+            [ pending cry whu
+            ]
+        ]
+    , Round 34
+        [ Day "Friday"
+            "2026-04-24"
+            [ pending sun nfo
+            ]
+        , Day "Saturday"
+            "2026-04-25"
+            [ pending ful avl
+            , pending bou lee
+            , pending liv cry
+            , pending whu eve
+            , pending wol tot
+            , pending ars new
+            ]
+        , Day "Sunday"
+            "2026-04-26"
+            [ pending bur mci
+            , pending bha che
+            ]
+        , Day "Monday"
+            "2026-04-27"
+            [ pending mun bre
+            ]
+        ]
+    , Round 35
+        [ Day "Saturday"
+            "2026-05-02"
+            [ pending bou cry
+            , pending ars ful
+            , pending avl tot
+            , pending bre whu
+            , pending che nfo
+            , pending eve mci
+            , pending lee bur
+            , pending mun liv
+            , pending new bha
+            , pending wol sun
+            ]
+        ]
+    , Round 36
+        [ Day "Saturday"
+            "2026-05-09"
+            [ pending bha wol
+            , pending bur avl
+            , pending cry eve
+            , pending ful bou
+            , pending liv che
+            , pending mci bre
+            , pending nfo new
+            , pending sun mun
+            , pending tot lee
+            , pending whu ars
+            ]
+        ]
+    , Round 37
+        [ Day "Sunday"
+            "2026-05-17"
+            [ pending bou mci
+            , pending ars bur
+            , pending avl liv
+            , pending bre cry
+            , pending che tot
+            , pending eve sun
+            , pending lee bha
+            , pending mun nfo
+            , pending new whu
+            , pending wol ful
+            ]
+        ]
+    , Round 38
+        [ Day "Sunday"
+            "2026-05-24"
+            [ pending bha mun
+            , pending bur wol
+            , pending cry ars
+            , pending ful new
+            , pending liv bre
+            , pending mci avl
+            , pending nfo bou
+            , pending sun che
+            , pending tot eve
+            , pending whu lee
             ]
         ]
     ]
